8aeaef91e20605486a6ce7daf2ff1bb5ae53a5da
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-12-08 14:33:25 -0600

    update to 1.21.3, part 2

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ git clone https://github.com/PaperMC/Folia #clone repo
 git config --global user.email "you@example.com" #idk why this here
 git config --global user.name "Your Name" #still dunno
 cd Folia #cd to repo
-git checkout dev/1.21.1 #checkout dev branch
-git pull origin dev/1.21.1 #pull dev branch
+git checkout dev/1.21.3 #checkout dev branch
+git pull origin dev/1.21.3 #pull dev branch
 ./gradlew applyPatches #apply patches
 ./gradlew createReobfPaperclipJar #create jar
